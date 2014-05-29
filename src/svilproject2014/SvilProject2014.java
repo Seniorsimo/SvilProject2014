d8286de75f659f79e9602997c583a23a738acd52
User <User@Simo-Station>
2014-05-29 16:50:23 +0200

    Tutto inizia da qui

---
+++ src/svilproject2014/SvilProject2014.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package svilproject2014;
+
+/**
+ *
+ * @author User
+ */
+public class SvilProject2014 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}

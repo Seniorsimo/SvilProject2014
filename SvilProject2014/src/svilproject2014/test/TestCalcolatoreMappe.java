794f5a43d90e3be091ac25eedcb53c4d43f8512e
User <User@Simo-Station>
2014-06-03 22:30:42 +0200

    Creati i 3 calcolatori

---
+++ SvilProject2014/src/svilproject2014/test/TestCalcolatoreMappe.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package svilproject2014.test;
+
+/**
+ *
+ * @author User
+ */
+public class TestCalcolatoreMappe {
+    public static void main(String[] args){
+        
+    }
+}

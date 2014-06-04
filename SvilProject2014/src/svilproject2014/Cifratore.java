c2dd3a7d758024fc51dec342769a96ff84fce1e5
Simone <seniorsimo@hotmail.it>
2014-06-04 19:46:29 +0200

    Qualche fix

--- SvilProject2014/src/svilproject2014/Cifratore.java
+++ SvilProject2014/src/svilproject2014/Cifratore.java
@@ -11,15 +11,29 @@ package svilproject2014;
 public class Cifratore {
     
     public static String cifra(Mappatura map, String testo){
-        //da implementare
+        char[] txt = testo.toCharArray();
+        char[] out = new char[txt.length];
         
-        return null;
+        for(int i=0; i<txt.length; i++){
+            out[i] = map.map(txt[i]);
+        }
+        
+        String converted = String.copyValueOf(out);
+        
+        return converted;
     }
     
     public static String decifra(Mappatura map, String testo){
-        //da implementare
+        char[] txt = testo.toCharArray();
+        char[] out = new char[txt.length];
         
-        return null;
+        for(int i=0; i<txt.length; i++){
+            out[i] = map.inverseMap(txt[i]);
+        }
+        
+        String converted = String.copyValueOf(out);
+        
+        return converted;
     }
     
 }
--- SvilProject2014/src/svilproject2014/Mappatura.java
+++ SvilProject2014/src/svilproject2014/Mappatura.java
@@ -12,22 +12,39 @@ public class Mappatura {
     
     private char[] mappa;
     private char[] mappaInversa;
+    //alfabeto di partenza
+    private char[] alfabeto = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     
     public char map(char c){
-        //da implementare
+        int position = getPosition(c, alfabeto);
         
-        return ' ';
+        //nel caso il char non sia presente restituisco il char iniziale
+        if(c<0) return c;
+        
+        //altrimenti restituisco il char associato
+        return mappa[position];
     }
     
     public char inverseMap(char c){
-        //da implementare
+        int position = getPosition(c, alfabeto);
         
-        return ' ';
+        //nel caso il char non sia presente restituisco il char iniziale
+        if(c<0) return c;
+        
+        //altrimenti restituisco il char associato
+        return mappaInversa[position];
     }
     
     public Mappatura(char[] map, char[] inverseMap){
         mappa = map;
         mappaInversa = inverseMap;
+    }
+    
+    private int getPosition(char c, char[] list){
+        for(int i=0; i<list.length; i++){
+            if(list[i]==c) return i;
+        }
+        return -1;
     }
     
 }
